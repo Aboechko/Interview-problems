87c9af3001d76ae73a37444238dcccf129624d96
A Boechko <46904893+Aboechko@users.noreply.github.com>
2020-02-09 15:18:41 -0500

    Create problemSet.js

---
+++ problemSet.js
@@ -0,0 +1,15 @@
+//reverse string
+reverse("text");
+
+function reverse(str) {
+	console.log(str.split("").reverse().join("");
+
+}
+
+//check string length
+var str = 'text';
+
+console.log(str.length);
+
+
+		
