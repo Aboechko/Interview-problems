6215237432435a81c422a1fa182dc24ae789aeeb
A Boechko <46904893+Aboechko@users.noreply.github.com>
2020-02-09 15:52:44 -0500

    Create problemSet.js

---
+++ problemSet.js
@@ -0,0 +1,20 @@
+//reverse string
+reverse("text");
+
+function reverse(str) {
+	console.log(str.split("").reverse().join("");
+
+}
+
+//check string length
+var str = 'text';
+
+console.log(str.length);
+
+//check if string length is divisible by 4. 
+
+var str = 'text';
+	if(str.length % 4 === 0);
+		console.log("The length of the string is divisible by 4");
+
+		
